2c2a3fac988ecb269a48ba548d848ae2dddfae92
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-27 20:45:10 -0500

    actualizacion del componente registro con sus estilos

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: ``
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
