9dca1859fa6acdd199799c860e2e2fa704d1a041
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-28 11:21:38 -0500

    asignacion de rutas para el directorio modular

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
