903ea4c3dc8077a590a899ad1b3e648da22d2921
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-27 20:50:18 -0500

    implementacion del page not found

--- src/app/pages/nopage-found/nopage-found.component.ts
+++ src/app/pages/nopage-found/nopage-found.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nopage-found',
   templateUrl: './nopage-found.component.html',
-  styles: ``
+  styleUrls: ['./nopage-found.component.css'],
 })
 export class NopageFoundComponent {
-
+  year: number = new Date().getFullYear();
 }
