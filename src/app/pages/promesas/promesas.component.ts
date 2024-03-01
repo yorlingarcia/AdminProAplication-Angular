7f0005cdd18956d593e531e231af2da329b6f99d
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-03-01 09:50:59 -0500

    implementacion de promesas

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -9,19 +9,28 @@ export class PromesasComponent implements OnInit {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const promesa = new Promise((resolve, reject) => {
-      if (true) {
-        resolve('Hola mundo');
-      } else {
-        reject('Algo salio mal');
-      }
-    });
+    // const promesa = new Promise((resolve, reject) => {
+    //   if (true) {
+    //     resolve('Hola mundo');
+    //   } else {
+    //     reject('Algo salio mal');
+    //   }
+    // });
+    // promesa
+    //   .then((mensaje) => {
+    //     console.log('termine!!', mensaje);
+    //   })
+    //   .catch((err) => console.log(err));
+    // console.log('Fin del OnInit');
+    // this.getUsuarios();
+    this.getUsuarios().then((usuarios) => console.log(usuarios));
+  }
 
-    promesa
-      .then((mensaje) => {
-        console.log('termine!!', mensaje);
-      })
-      .catch((err) => console.log(err));
-    console.log('Fin del OnInit');
+  getUsuarios() {
+    return new Promise((resolve) => {
+      fetch('https://reqres.in/api/users?page=2')
+        .then((resp) => resp.json())
+        .then((body) => resolve(body.data));
+    });
   }
 }
