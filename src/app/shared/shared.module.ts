01a2b5a0724b3c66d1e527e71ae2b3913ca8208a
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-29 22:22:48 -0500

    rutas de acceso a account-settings por medio de routerlink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [SidebarComponent, HeaderComponent, BreadcrumbsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SidebarComponent, HeaderComponent, BreadcrumbsComponent],
 })
 export class SharedModule {}
