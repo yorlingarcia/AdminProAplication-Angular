1171ef3e3a11c0f94c8478bd900598fd2f9e1c1f
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-03-01 14:26:33 -0500

    implementacion de observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Observable, retry } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RxjsComponent {
+  constructor() {
+    this.retornaObservable()
+      .pipe(retry(1))
+      .subscribe(
+        (resp) => console.log('subs: ', resp),
+        (err) => console.warn(err),
+        () => console.info('obs terminado')
+      );
+  }
 
+  retornaObservable(): Observable<number> {
+    let i = -1;
+    const obs$ = new Observable<number>((observer) => {
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('Llegp al error');
+        }
+      }, 1000);
+    });
+    return obs$;
+  }
 }
