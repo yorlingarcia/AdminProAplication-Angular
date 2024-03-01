b8972e2252fa03d17fe6bd221c78c7fcc65ce2a3
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-29 22:40:13 -0500

    creacion de servicio para la lectura de items en el dashboard y cambio en la direccion de la ruta de cerrar sesion

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SidebarService {
+  menu: any[] = [
+    {
+      titulo: 'DashBoard',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Principal', url: '/' },
+        { titulo: 'Progresbar', url: 'progress' },
+        { titulo: 'Graficas', url: 'grafica1' },
+      ],
+    },
+  ];
+
+  constructor() {}
+}
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SidebarComponent {
-
+  meniItems: any[];
+  constructor(private sidebarService: SidebarService) {
+    this.meniItems = sidebarService.menu;
+  }
 }
