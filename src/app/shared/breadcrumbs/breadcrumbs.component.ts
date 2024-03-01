9b8acad3c27b930f1b3c548407e79aafe82ade71
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-03-01 17:04:19 -0500

    implementacion del ngOndestroy

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
-import { ActivatedRouteSnapshot, ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  ActivationEnd,
+  Data,
+  Router,
+} from '@angular/router';
+import { Observable, Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: ``,
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
   public titulo: string = '';
+  public tituloSubs$: Subscription;
   constructor(private router: Router) {
-    this.router.events
-      .pipe(
-        filter((event: any) => event instanceof ActivationEnd),
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe(({ titulo }) => {
-        this.titulo = titulo;
-        document.title = titulo;
-      });
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta(): Observable<Data> {
+    return this.router.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
   }
 }
