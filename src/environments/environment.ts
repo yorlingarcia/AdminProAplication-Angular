bb3383141b32311a6086015706d3da337450c8fe
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-03-10 16:22:30 -0500

    uso de link de acceso al backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   produtcion: false,
-  base_url: 'http://localhost:3000/api',
+  // base_url: 'http://localhost:3000/api',
+  base_url: 'https://adminproaplication-node-production.up.railway.app/api',
 };
