b580d31936a133ffe5c8fec9116c519256076091
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-10-18 12:07:55 -0500

    actualizacion de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   produtcion: false,
-  base_url: 'http://localhost:3000/api',
+  //base_url: 'http://localhost:3000/api',
   // base_url: 'https://adminproaplication-node-production.up.railway.app/api',
+  base_url: 'https://appuniversal.onrender.com/api',
 };
